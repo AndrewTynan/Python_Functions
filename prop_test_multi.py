c0037c68710045b8cb5f08d52d68df4786fdd813
atynan <Andrew@Deliv.co>
2024-03-18 17:05:19 -0600

    Update prop_test_multi.py
    revisions

--- prop_test_multi.py
+++ prop_test_multi.py
@@ -1,26 +1,27 @@
 
 from statsmodels.stats.proportion import test_proportions_2indep, confint_proportions_2indep 
 
-# DEFINE prop_test() 
 
-def prop_test(df, month): 
-    # NOTE month is just for adding a data labels in params    
+def prop_test(df, month, group_var): 
+    # NOTE month is just for adding a data labels in params   
 
-    ucg_retained = int(df.query("is_longterm_ucg == 'Yes'").loc[:,['retained_count_total']].values[0])
-    ucg_total    = int(df.query("is_longterm_ucg == 'Yes'").loc[:,['cohort_count_total']].values[0])
+    # group_var conditon needs to be adjusted 
 
-    non_ucg_retained = int(df.query("is_longterm_ucg == 'No'").loc[:,['retained_count_total']].values[0])
-    non_ucg_total    = int(df.query("is_longterm_ucg == 'No'").loc[:,['cohort_count_total']].values[0])    
+    retained = int(df.query("group_var == 'Yes'").loc[:,['retained_count_total']].values[0])
+    total    = int(df.query("group_var == 'Yes'").loc[:,['cohort_count_total']].values[0])
 
-    prop_test = test_proportions_2indep(count1      = non_ucg_retained, 
-                                        nobs1       = non_ucg_total,
-                                        count2      = ucg_retained,
-                                        nobs2       = ucg_total) # alternative = 'larger') 
+    non_retained = int(df.query("group_var == 'No'").loc[:,['retained_count_total']].values[0])
+    non_total    = int(df.query("group_var == 'No'").loc[:,['cohort_count_total']].values[0])    
 
-    prop_test_ci = confint_proportions_2indep(count1 = non_ucg_retained,
-                                              nobs1  = non_ucg_total,
-                                              count2 = ucg_retained,
-                                              nobs2  = ucg_total) 
+    prop_test = test_proportions_2indep(count1      = non_retained, 
+                                        nobs1       = non_total,
+                                        count2      = retained,
+                                        nobs2       = total) # alternative = 'larger') 
+
+    prop_test_ci = confint_proportions_2indep(count1 = non_retained,
+                                              nobs1  = non_total,
+                                              count2 = retained,
+                                              nobs2  = total) 
 
     prop_test_output = pd.DataFrame({'test':      ['prop test'],
                                      'statistic': [prop_test.statistic],
@@ -32,11 +33,11 @@ def prop_test(df, month):
     prop_test_output = pd.concat([params, prop_test_output], axis=1)  
 
     return prop_test_output 
+    
 
+def prop_test_runner(df, group_var): 
 
-# DEFINE prop_test_runner() 
-
-def prop_test_runner(df): 
+    # group_var conditon needs to be adjusted 
 
     lifetime_months = df.loc[:, 'lifetime_month'].unique()
 
@@ -46,8 +47,8 @@ def prop_test_runner(df):
 
         df_agg = (df
                     .query(lifetime_months_var) 
-                    .loc[:, ['lifetime_month', 'is_longterm_ucg', 'cohort_count', 'retained_count']]
-                    .groupby(['lifetime_month', 'is_longterm_ucg'])
+                    .loc[:, ['lifetime_month', 'group_var', 'cohort_count', 'retained_count']]
+                    .groupby(['lifetime_month', 'group_var'])
                     .agg(cohort_count_total  = ('cohort_count', 'sum'),
                         retained_count_total = ('retained_count', 'sum'))
                     .reset_index())  
@@ -67,8 +68,3 @@ def prop_test_runner(df):
             df_agg_results    = pd.concat([df_agg_results, df_agg], ignore_index=True, axis=0) 
 
     return prop_test_results, df_agg_results 
-
-
-
-
-
